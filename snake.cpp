ca99412cc81075a3deddefa0c8ff95f2ac8ef00a
Ernold Puvlist <epuvlist@gmail.com>
2023-05-29 15:31:37 +0100

    Much work on snake class and movement detection

--- snake.cpp
+++ snake.cpp
@@ -1,3 +1,8 @@
+// snake.cpp
+// =========
+//
+// Snake game using ncurses
+
 #include <ncurses.h>
 // #include <unistd.h> // needed if using sleep() or usleep()
 
@@ -6,62 +11,82 @@ static const char snake_piece = '*',
     food = 'X';
 const int game_speed = 10; // Parameter for halfdelay function in tenths of a second
 
+// Define Coord structure, to hold row & col coordinates
+struct Coord {  // Struct that contains a pair of screen coordinates (y, x)
+        int row, col;
+};
+
 // Class definition
 class Snake {
 
-    struct Coord {  // Struct that contains a pair of screen coordinates (y, x)
-        int y, x;
-    };
-
+    WINDOW *win;    // The window to display the snake in
     Coord *coords;  // Pointer to an array of coordinates
                     // The array is allocated at runtime in the constructor.
 
     int head, tail; // Indices into the coords, identifying where the head and tail are.
+    int max_length; // Maximum length (also the size of the coords array)
+
+    int max_row, max_col; // Maximum row & col position
+
     int direction;  // Direction the snake is moving in. For this we will
                     // use the constants KEY_UP, KEY_LEFT etc.
-    int max_length; // Maximum length (also the size of the coords array)
 
 public:
     // Constructor. 'size' is the maximum size the snake is allowed to grow to.
-    Snake(int size) {
-        coords = new Coord[size];
+    Snake(WINDOW *w) {
+        win = w;
+        getmaxyx(w, max_row, max_col);
+        max_length = max_row * max_col;
+        coords = new Coord[max_length];
         head = 0;
         tail = 0;
         direction = KEY_LEFT;
-        max_length = size;
     };
 
     ~Snake() {  // Default destructor
         delete coords;
     };
-
-    int row() {
-        // Return the snake's current row on screen
-        return coords[head].y;
+/* Not needed?
+    Coord *get_position() {
+        // Return the snake head's current position on screen
+        return &coords[head];
+    }
+*/
+    int get_direction() {
+        return direction;
     }
 
-    int col() {
-        // Return the snake's current column on screen
-        return coords[head].x;
-        }
+    void set_direction(int d) {
+        direction = d;
+    }
 
-    void start(WINDOW *win) {
+    void start() {
         // Display a new snake (i.e. head/tail only).
         // Set the coordinates as the middle of the window
 
-        int rows, cols;
-        getmaxyx(win, rows, cols);
-        coords[0].y = rows / 2;
-        coords[0].x = cols / 2;
-        mvwaddch(win, coords[0].y, coords[0].x, snake_piece);
+        coords[0].row = max_row / 2;
+        coords[0].col = max_col / 2;
+        mvwaddch(win, coords[0].row, coords[0].col, snake_piece);
         wrefresh(win);
     };
 
-    void advance(WINDOW *win) {
+    int out_of_bounds() {
+        // Check if snake is about to go out of bounds
+        int row = coords[head].row, col = coords[head].col;
+
+        if ((row == 0 and direction == KEY_UP) or
+            (row == max_row-1 and direction == KEY_DOWN) or
+            (col == 0 and direction == KEY_LEFT) or
+            (col == max_col-1 and direction == KEY_RIGHT))
+            return 1;
+        else return 0;
+    }
+
+    void advance() {
         // Advance the snake by adding a new head, and deleting old tail if needed.
         // Also update the coords array, which tracks where the snake has been.
 
-        int old_row = coords[head].y, old_col = coords[head].x;
+        int old_row = coords[head].row, old_col = coords[head].col;
         
         // Advance the head in the coordinates array.
         // If the head reaches the end of the array, then start again
@@ -72,19 +97,18 @@ public:
  
         switch(direction) {
             case KEY_LEFT:
-                coords[head].y = old_row;
-                coords[head].x = old_col - 1;
+                coords[head].row = old_row;
+                coords[head].col = old_col - 1;
 
                 if ((winch(win) & A_CHARTEXT) != food) {
                     // No food here, so remove the tail
-                    mvwaddch(win, coords[tail].y, coords[tail].x, ' ');
+                    mvwaddch(win, coords[tail].row, coords[tail].col, ' ');
                     // Advance the tail in the coordinates array
                     tail = (tail + 1) % max_length;  
                 }
                 break;
             case KEY_RIGHT:
-                coords[head].y = old_row;
-                coords[head].x = old_col + 1;
+
                 break;
             case KEY_UP:
             case KEY_DOWN:
@@ -92,7 +116,7 @@ public:
         };
 
         // Plot new head piece
-        mvwaddch(win, coords[head].y, coords[head].x, snake_piece);
+        mvwaddch(win, coords[head].row, coords[head].col, snake_piece);
     };
 };
 
@@ -134,25 +158,29 @@ int main() {
     // Maximum length is gamewin rows x gamewin columns
     int gmrows, gmcols;
     getmaxyx(gamewin, gmrows, gmcols);
-    Snake snake(gmrows * gmcols);
+    Snake snake(gamewin);
 
-    snake.start(gamewin);
-
-#ifdef DEBUG
-    wgetch(gamewin);
-    snake.advance(gamewin);
-    wgetch(gamewin);
-#endif
+    snake.start();
 
     // Main program loop
-/*
-    int c;  // character input
 
-    while(TRUE) {
-        c = getch();    // Read any keystroke into buffer
+    int c;  // keyboard input
+    int game_over = 0;  // When becomes 1, game over
+    // Coord *rowcol;
+    int direction;
+
+    while(!game_over) {
+        c = getch();    // Read keystroke into buffer
+
+        direction = snake.get_direction();
+
         switch(c) {
             case KEY_UP:
-                mvprintw(0, 0, "UP   ");
+                if (direction == KEY_DOWN) { // Crashed back onto yourself
+                    game_over = 1;
+                    break;
+                }
+                snake.set_direction(KEY_UP);
                 break;
             case KEY_DOWN:
                 mvprintw(0, 0, "DOWN ");
@@ -166,10 +194,19 @@ int main() {
         if (c == 'q' or c == 'Q')
             break;
 
+        // Check for moving out of bounds
+        if (snake.out_of_bounds())
+            game_over = 1;
+
+        if (!game_over)
+            snake.advance();
+        
         refresh();
         wrefresh(gamewin);
     }
-*/
+
+/* STUB - 'Game Over' point here */
+
     // Cleanup and exit
     delwin(gamewin);
     endwin();
